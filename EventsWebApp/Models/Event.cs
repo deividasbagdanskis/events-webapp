792d141cef601ea64ed9ebc55a424bae24da9091
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-12-12 14:16:59 +0200

    Create event model

---
+++ EventsWebApp/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EventsWebApp.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string ImageName { get; set; }
+        public DateTime DateAndTime { get; set; }
+    }
+}
