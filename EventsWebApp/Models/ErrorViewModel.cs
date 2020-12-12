1141a6bb6d13158f9b0b3fcd4d28d9fbed037395
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-12-12 12:45:06 +0200

    Create a starter ASP.NET MVC project

---
+++ EventsWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EventsWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
