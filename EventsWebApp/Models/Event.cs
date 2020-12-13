f21d977cb51e5be6cc3bb720546654fb44b18ee5
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-12-13 19:19:03 +0200

    Add User to Event model class

--- EventsWebApp/Models/Event.cs
+++ EventsWebApp/Models/Event.cs
@@ -6,6 +6,8 @@ namespace EventsWebApp.Models
     public class Event
     {
         public int Id { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int CategoryId { get; set; }
