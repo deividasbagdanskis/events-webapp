012c68a59738955595232232a19cc87dd652e224
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-12-13 18:52:33 +0200

    Create Events controller and index view

---
+++ EventsWebApp/Controllers/EventsController.cs
@@ -0,0 +1,107 @@
+﻿using EventsWebApp.Context;
+using EventsWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsWebApp.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly EventsWebAppContext _context;
+
+        public EventsController(EventsWebAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EventsController
+        [Authorize]
+        public async Task<ActionResult> Index(string city = null, int categoryId = 0, int date = 0)
+        {
+            var events = await _context.Event.Include(e => e.Category).Include(e => e.EventAttendees).ToListAsync();
+            var categories = await _context.Category.ToListAsync();
+
+            categories.Insert(0, new Category() { Id = 0, Name = "All" });
+
+            ViewData["Categories"] = new SelectList(categories, "Id", "Name");
+
+            return View();
+        }
+
+        // GET: EventsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EventsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EventsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EventsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
