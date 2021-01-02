6e05e3446fed82414ac693d445a3486a70dfa174
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2021-01-02 16:39:37 +0200

    Create image helper class

---
+++ EventsWebApp/Helpers/ImageHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace EventsWebApp.Helpers
+{
+    public class ImageHelper : IImageHelper
+    {
+        private string _imageFolder;
+
+        public ImageHelper(IWebHostEnvironment webHostEnvironment)
+        {
+            _imageFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+        }
+
+        public string Save(IFormFile imageFile)
+        {
+            string uniqueFileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
+            string filePath = Path.Combine(_imageFolder, uniqueFileName);
+
+            using FileStream fileStream = new FileStream(filePath, FileMode.Create);
+
+            imageFile.CopyTo(fileStream);
+
+            return uniqueFileName;
+        }
+
+        public void Delete(string imageName)
+        {
+            string imagePath = Path.Combine(_imageFolder, imageName);
+
+            FileInfo fileInfo = new FileInfo(imagePath);
+
+            fileInfo.Delete();
+        }
+    }
+}
