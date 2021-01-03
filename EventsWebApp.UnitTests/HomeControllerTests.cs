09a4aa602f187ffcb2159c08da6dac397518fc64
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2021-01-03 18:19:44 +0200

    Create HomeController unit test

---
+++ EventsWebApp.UnitTests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using EventsWebApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace EventsWebApp.UnitTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Pass()
+        {
+            HomeController homeController = new HomeController();
+
+            var result = homeController.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
