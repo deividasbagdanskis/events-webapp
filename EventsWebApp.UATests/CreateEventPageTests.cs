fc234273d825a0b40cdb8a35158647f440e8c9f1
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2021-01-07 16:14:21 +0200

    Create CreateEvent page UI tests

---
+++ EventsWebApp.UATests/CreateEventPageTests.cs
@@ -0,0 +1,90 @@
+﻿using EventsWebApp.UATests.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace EventsWebApp.UATests
+{
+    public class CreateEventPageTests
+    {
+        private readonly FirefoxOptions _options;
+
+        public CreateEventPageTests()
+        {
+            _options = new FirefoxOptions
+            {
+                AcceptInsecureCertificates = true
+            };
+        }
+
+        [Fact]
+        public void CreateEvent_ValidInputs_ReturnsCreatedEventsName()
+        {
+            using IWebDriver webDriver = new FirefoxDriver(_options);
+
+            LoginPage loginPage = new LoginPage(webDriver);
+            loginPage.LoginUser("v.pavardenis@gmail.com", "password");
+
+            CreateEventPage createEventPage = new CreateEventPage(webDriver);
+
+            string name = "Test event";
+            string description = "Proin at lorem gravida, lacinia dolor quis, sodales quam. Vestibulum in purus " +
+                "est. Quisque turpis mi, hendrerit at aliquet vitae, porttitor commodo metus.";
+            string category = "Arts";
+            string city = "Vilnius";
+            string address = "Medziu g. 15";
+
+            FileInfo fileInfo = new FileInfo(@"..\..\..\event_image.png");
+
+            string imageFilePath = fileInfo.FullName;
+            DateTime date = DateTime.Today.AddDays(5);
+            TimeSpan time = new TimeSpan(17, 0, 0);
+
+            createEventPage.CreateEvent(name, description, category, city, address, imageFilePath, date, time);
+
+            string expectedEventName = name;
+            string actualEventName = createEventPage.GetCreatedEventName(name);
+
+            Assert.Equal(expectedEventName, actualEventName);
+
+            DeleteEventPage deleteEventPage = new DeleteEventPage(webDriver);
+            deleteEventPage.DeleteEvent();
+        }
+
+        [Fact]
+        public void CreateEvent_InvalidInputs_ReturnsErrorMessages()
+        {
+            using IWebDriver webDriver = new FirefoxDriver(_options);
+
+            LoginPage loginPage = new LoginPage(webDriver);
+            loginPage.LoginUser("v.pavardenis@gmail.com", "password");
+
+            CreateEventPage createEventPage = new CreateEventPage(webDriver);
+
+            string name = "";
+            string description = "";
+            string category = "Music";
+            string city = "";
+            string address = "";
+            string imageFilePath = "";
+            DateTime date = DateTime.Today.AddDays(5);
+            TimeSpan time = new TimeSpan(17, 0, 0);
+
+            createEventPage.CreateEvent(name, description, category, city, address, imageFilePath, date, time);
+
+            List<string> expectedErrorMessages = new List<string>() 
+            {
+                "The Name field is required.",
+                "The Description field is required.",
+                "The City field is required.",
+                "The Address field is required."
+            };
+            List<string> actualErrorMessages = createEventPage.GetErrorMessages();
+
+            Assert.Equal(expectedErrorMessages, actualErrorMessages);
+        }
+    }
+}
---
+++ EventsWebApp.UATests/PageObjects/DeleteEventPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+
+namespace EventsWebApp.UATests.PageObjects
+{
+    public class DeleteEventPage
+    {
+        private readonly IWebDriver _webDriver;
+        private readonly string _usersEventsPageUrl = "https://localhost:44357/Events/IndexUserEvents";
+        private readonly By _createdEventNameLink = By.XPath("/html/body/div/main/table[2]/tbody/tr/td[1]/a");
+        private readonly By _deleteEventPageLink = By.XPath("/html/body/div/main/div/div[4]/a[2]");
+        private readonly By _deleteEventButton = By.XPath("/html/body/div/main/div/div[4]/form/input[1]");
+
+        public DeleteEventPage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public void DeleteEvent()
+        {
+            _webDriver.Navigate().GoToUrl(_usersEventsPageUrl);
+
+            _webDriver.FindElement(_createdEventNameLink).Click();
+            _webDriver.FindElement(_deleteEventPageLink).Click();
+            _webDriver.FindElement(_deleteEventButton).Click();
+        }
+
+        public bool CheckIfEventWasDeleted(string name)
+        {
+            bool wasDeleted = false;
+
+            By deletedEventRecord = By.XPath($"//td[normalize-space() = '{name}']");
+
+            try
+            {
+                IWebElement element = _webDriver.FindElement(deletedEventRecord);
+            }
+            catch (NoSuchElementException)
+            {
+                wasDeleted = true;
+            }
+
+            return wasDeleted;
+        }
+    }
+}
